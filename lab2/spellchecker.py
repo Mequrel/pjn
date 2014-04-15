159e80e37b7ecafc63f6db1106409fe2f1553d34
mequrel <meq@meq.com>
2014-04-15 20:45:11 +0200

    Reading words from file

--- lab2/spellchecker.py
+++ lab2/spellchecker.py
@@ -1,6 +1,15 @@
 #!/usr/bin/env python
 
+import fileinput
+
+
+def main():
+    for word in fileinput.input():
+        print propose(word)
+
 
 def propose(word):
     return word
 
+if __name__ == "__main__":
+    main()
