b060426a71d1a0e54e1b984b92b54cd973cfa55c
mequrel <meq@meq.com>
2014-04-15 21:37:33 +0200

    Dictionary added

--- lab2/spellchecker.py
+++ lab2/spellchecker.py
@@ -1,10 +1,24 @@
 #!/usr/bin/env python
 
 import fileinput
+from itertools import chain
+
+DICTIONARY_PATH = 'dictionary.txt'
+
+
+def load_dictionary():
+    with open(DICTIONARY_PATH) as f:
+        #lines = f.readlines()
+        lines = f.read().splitlines()
+        split = map(lambda x: x.split(", "), lines)
+        return chain.from_iterable(split)
 
 
 def main():
+    dictionary = list(load_dictionary())
+
     for word in fileinput.input():
+        word = word.strip()
         print propose(word)
 
 
