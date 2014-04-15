8554406cb53f194c1409cc02f4ad840bb8aacbbb
mequrel <meq@meq.com>
2014-04-15 23:00:47 +0200

    Dictionary converted to utf-8

--- lab2/dict.sh
+++ lab2/dict.sh
@@ -2,4 +2,6 @@ wget http://sjp.pl/slownik/odmiany/sjp-odm-20140112.zip
 unzip sjp-odm-20140112.zip
 rm README.txt
 rm sjp-odm-20140112.zip
-mv odm.txt dictionary.txt
+mv odm.txt dictionary.win1250.txt
+iconv -f windows-1250 -t UTF-8 dictionary.win1250.txt > dictionary.txt
+rm dictionary.win1250.txt
