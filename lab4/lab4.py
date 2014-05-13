b58f0c0ddc9e8a3ae96fcba877d68477d500e2ab
mequrel <meq@meq.com>
2014-05-13 23:41:58 +0200

    a tergo dict added

--- lab4/lab4.py
+++ lab4/lab4.py
@@ -4,10 +4,29 @@ import fileinput
 from plp import PLP
 
 
+def safe_forms(p, i):
+    try:
+        return p.forms(i)
+    except UnicodeDecodeError:
+        return []
+
+
 def main():
     words = [line.decode('utf-8').strip() for line in fileinput.input()]
 
     p = PLP()
+    p.safe_forms = safe_forms
+
+    i = 16777216
+    #last = 18663968
+    last = 16780000
+
+    a_tergo = set()
+    while i <= last:
+        forms = safe_forms(p, i)
+        forms = map(lambda form: form[::-1], forms)
+        a_tergo.update(forms)
+        i += 1
 
     for word in words:
         stemmed_word = word
