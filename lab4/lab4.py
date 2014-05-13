5e53c74ccb9b5a27cbc61e915211668f4351f6e6
mequrel <meq@meq.com>
2014-05-13 18:19:23 +0200

    Simple PLP stemming

--- lab4/lab4.py
+++ lab4/lab4.py
@@ -1,13 +1,22 @@
 #!/usr/bin/env python
 
 import fileinput
+from plp import PLP
 
 
 def main():
-    words = [line.strip() for line in fileinput.input()]
+    words = [line.decode('utf-8').strip() for line in fileinput.input()]
+
+    p = PLP()
 
     for word in words:
-        print "{0},{1}".format(word, word)
+        stemmed_word = word
+
+        rec_list = p.rec(word)
+        if rec_list:
+            stemmed_word = p.bform(rec_list[0])
+
+        print "{0},{1}".format(word.encode("utf-8"), stemmed_word.encode("utf-8"))
 
 if __name__ == '__main__':
     main()
