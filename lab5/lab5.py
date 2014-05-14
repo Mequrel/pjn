f00d86eb76e533258b9191c550f2a76d43c0bef2
mequrel <meq@meq.com>
2014-05-14 17:48:58 +0200

    Counting words

--- lab5/lab5.py
+++ lab5/lab5.py
@@ -4,20 +4,20 @@ import fileinput
 import pickle
 from words import extract_words
 from stemming import stem
+from collections import Counter
 
 
 def main():
-    words = extract_words(fileinput.input())
-
     with open('atergo_all.pickled') as atergo_file:
         a_tergo = pickle.load(atergo_file)
 
-    for word in list(words)[:100]:
-        print stem(a_tergo, word), " ", word
-        # add plp
+    words = extract_words(fileinput.input())
+    words = [stem(a_tergo, word) for word in words]
 
+    frequencies = Counter(words).most_common()
 
-
+    for word, count in frequencies:
+        print("{} {}".format(count, word.encode('utf-8')))
 
 
 if __name__ == '__main__':
